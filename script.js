46c50c7f6db3a8e3b5530d3e3568af1e3405732c
Ayoub <akhribi627@gmail.com>
2025-06-27 16:59:19 +0100

    started on the to do list

---
+++ script.js
@@ -0,0 +1,10 @@
+listenerdocument.addEventListener('DOMContentLoaded', function () {
+  const addButton = document.getElementById('add-task-btn');
+  const taskInput = document.getElementById('task-input');
+  const taskList = document.getElementById('task-list');
+
+  function addTask() {
+    
+  }
+
+})
